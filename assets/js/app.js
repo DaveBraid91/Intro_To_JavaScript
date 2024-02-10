5a36b6d570a4f96afe75e633a32ac24ef9a0d72e
DaveBraid91 <d.aragones91@gmail.com>
2024-02-10 21:31:43 +0100

    First Commit

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+//Así se declara una variable
+let n = 9;
+//Se imprime la variable "n" en la consola
+console.log(n);
+
+//Tipos de variable predefinidos
+//Números
+let myNumber = 5;
+let myNumber2 = 5.55555555;
+console.log(typeof(myNumber));
+console.log(typeof(myNumber2));
+
+//¿Qué puñeta es un NaN?
+let number = 0/0;
+console.log(number + 45);
+console.log(typeof(NaN));
+
+//Booleanos
+let myBool = true;
+let myBool2 = false;
+
+//String
+
+//Undefined
+
+//Null
+
+//Operadores
+// Suma + 
+// Resta -
+// Multiplicación *
+// División /
+// Módulo %
+// Exponente **
+// Añadir 1 n++, ++n
+// Restar 1 n--, --n;
+
+console.log("El módulo de 22 entre 2 es: " + 22 % 2);
+
+console.log("7 elevado a 12 es: " + 7 ** 12);
+
+n = 4;
+
+console.log(++n);
+console.log(n);
+
+
+
+/*
+n *= 2;
+
+console.log(n);
+
+n += 1;
+
+console.log(n);
+
+n = Math.floor(n);
+
+console.log(n);
+*/
